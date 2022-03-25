9b6c32ab9320732a0eb4845e260d4fd62ed6b07b
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-03-25 21:37:35 +0100

    Implemented login

--- thshop-middleware/routes/auth/login.ts
+++ thshop-middleware/routes/auth/login.ts
@@ -1,14 +1,62 @@
 import express from "express";
 const router = express.Router();
 
+import crypto from "crypto-js";
+import jwt from "jsonwebtoken";
+
 import { executeQuery } from "../../scripts/database";
+
+const tokenExpire: string = process.env.TOKEN_EXPIRE;
 
 router.post("/", async (req: any, res: any) => {
   try {
-    const data = await executeQuery("SELECT * from thshop.users");
+    if (!req.body || !req.body.password || !req.body.username)
+      throw new Error("Missing password or username");
 
-    res.status(200).json({ status: "success", message: "", data });
+    let data = await executeQuery(
+      "select username, passwordsalt from thshop.users where username = ?;",
+      req.body.username
+    );
+
+    if (data.length === 0) throw new Error("User could not be found!");
+    if (data.length > 1) throw new Error("Multiple users found!");
+    if (!data[0].passwordsalt) throw new Error("Salt could not be found!");
+
+    // generate password hash => sha256(password + salt)
+    const hash = crypto.enc.Base64.stringify(
+      crypto.SHA256(req.body.password + data[0].passwordsalt)
+    );
+
+    // check if user is valid
+    data = await executeQuery(
+      "select active, username, passwordhash from thshop.users where username = ? and passwordhash = ?;",
+      [req.body.username, hash]
+    );
+
+    if (data.length === 0) throw new Error("Invalid password!");
+    if (data.length > 1) throw new Error("Identical users found!");
+    if (data[0].active === 0) throw new Error("Account deactivated!");
+
+    // set users last login date
+    await executeQuery(
+      "update thshop.users set lastLogin = now() where username = ?;",
+      req.body.username
+    );
+
+    // // sign userdata token
+    const token = jwt.sign(
+      { username: req.body.username },
+      process.env.JWT_SECRET,
+      { expiresIn: tokenExpire }
+    );
+
+    res.status(200).json({
+      status: "success",
+      message: "Signed in as user " + req.body.username,
+      token,
+    });
   } catch (error) {
+    console.log(error);
     res.status(400).json({ status: "error", message: error.toString() });
   }
 });

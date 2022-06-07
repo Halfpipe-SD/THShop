6da41c44a3bdea02437e9043b39121af5cee48b1
Julian 41455100+thieleju@users.noreply.github.com <thiele.juli@gmail.com>
2022-06-07 17:39:56 +0200

    Added SQL Queries for monday

---
+++ Anfragen.sql
@@ -0,0 +1,109 @@
+-- Als Gast möchte ich alle Artikel Items angezeigt bekommen 
+-- TODO delete stockAvailable
+select i.name,
+  i.description,
+  i.price,
+  i.stock,
+  i.manufacturer
+from items i;
+
+-- Als Gast möchte ich nach einem bestimmten Artikel suchen
+SELECT i.name,
+  i.description,
+  i.price,
+  i.stock,
+  i.manufacturer
+from items i
+where i.name like "%?%";
+
+-- Als Gast möchte ich mich einloggen, um eine Bestellung zu tätigen.
+select username,
+  passwordsalt
+from users
+where username = ?;
+
+-- Als Mitglied möchte ich meine Email-Adrese bearbeiten, weil sie aktualisiert hat.
+update users u
+set u.email = ?
+where u.userid = ?;
+
+-- Als Mitglied möchte ich mein usernamen bearbeiten, weil dieser sich geändert hat.
+update users u
+set u.username = ?
+where u.userid = ?;
+-- Als Mitglied möchte ich mein passwort ändern, um die Sicherheit zu gewährleisten.
+update users u
+set u.passwordHash = ?
+where u.userid = ?;
+
+-- Als Mitglied möchte ich meinen Vor- und Nachnamen bearbeiten, da er sich geändert hat.
+update users u
+set u.firstname = ?,
+  u.lastname = ?
+where u.userid = ?;
+
+-- Als Mitglied möchte ich meine Telefonnummer/Alt bearbeiten, weil sie aktualisiert haben.
+update users u
+set u.phone = ?,
+  u.phoneAlt = ?
+where u.userid = ?;
+
+-- Als Mitglied möchte ich meine Adresse bearbeiten, weil sie sich geändert hat.
+update addresses a
+  join addressTypes adt on adt.addressID = a.addressID
+  join users u on u.userid = adt.userID
+set a.country = ?,
+  a.city = ?,
+  a.zipcode = ?,
+  a.street = ?
+where u.userid = ?;
+
+-- Als Mitglied möchte ich eine weitere Adresse hinzufügen, um eine weiter Zustellungsadresse zur Auswahl zu haben
+-- TODO 
+BEGIN transaction
+insert into addresses(country, city, zipcode, street)
+  join addressTypes adt on adt.addressID = a.addressID
+  join users u on u.userid = adt.userID
+values ("DE", "Schlumpfhausen", "43534", "Straße 1");
+insert into addressesTypes (
+    userID,
+    addressID,
+    isBillingAddress,
+    isDeliveryAddress
+  )
+  join addresseson adt.addressID = a.addressID
+  join users u on u.userid = adt.userID
+values (0,, 1, 1);
+commit;
+
+-- Als Mitglied möchte ich einen Artikel bestellen, den ich mir ausgesucht habe
+
+-- Als Mitglied möchte ich einen Artikel in den Warenkorb legen, um ihn zu kaufen
+
+-- Als Mitarbeiter möchte ich einen Artikel hinzufügen
+insert into items(
+    name,
+    dealerID,
+    description,
+    price,
+    stock,
+    manufacturer,
+    dateAdded
+  )
+values (
+    "Fußball 2",
+    1,
+    "ein weiterer ball",
+    59.99,
+    10,
+    "Adidas",
+    now()
+  );
+
+-- Als Mitarbeiter möchte ich einen Dealer hinzufügen
+insert into dealers(addressID, name)
+values (?, ?);
+
+-- Als Mitarbeiter möchte ich einen Artikel löschen (15)
+delete from items
+where itemID = ?;
